daf9343952c64f93b7209dfc3a17209c5b72cce6
faskarovv <145225660+faskarovv@users.noreply.github.com>
2025-04-08 01:30:34 +0400

    changed the name from old project

--- src/main/java/com/example/imageProcessing/ImageProccesingApp.java
+++ src/main/java/com/example/imageProcessing/ImageProccesingApp.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceAppApplication {
+public class ImageProccesingApp {
 
     public static void main(String[] args) {
 
 
-        SpringApplication.run(EcommerceAppApplication.class, args);
+        SpringApplication.run(ImageProccesingApp.class, args);
     }
 
 }
